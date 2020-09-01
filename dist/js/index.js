2a503b9fc77c7a4bca15c710c07780acb5389922
Alexey Iskhakov <a.raiondesu@gmail.com>
2020-09-01 03:58:25 +0300

    Update dist

--- dist/js/index.js
+++ dist/js/index.js
@@ -1,5 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+exports.withProps = exports.prop = void 0;
 exports.prop = (options) => options;
 exports.withProps = (props, setup) => {
     setup.props = props;
--- dist/minimal.js
+++ dist/minimal.js
@@ -1,2 +1,2 @@
-!function(e,t){"object"==typeof exports&&"undefined"!=typeof module?t(exports):"function"==typeof define&&define.amd?define(["exports"],t):t((e=e||self)["vue-functional-props"]={})}(this,function(e){"use strict";e.prop=function(e){return e},e.withProps=function(e,t){return t.props=e,t},Object.defineProperty(e,"__esModule",{value:!0})});
+!function(e,t){"object"==typeof exports&&"undefined"!=typeof module?t(exports):"function"==typeof define&&define.amd?define(["exports"],t):t((e=e||self)["vue-functional-props"]={})}(this,(function(e){"use strict";e.prop=function(e){return e},e.withProps=function(e,t){return t.props=e,t},Object.defineProperty(e,"__esModule",{value:!0})}));
 //# sourceMappingURL=minimal.js.map
