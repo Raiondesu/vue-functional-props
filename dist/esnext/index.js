78877914e531723a64476dbd3f818ece9a5cbf66
Raiondesu <a.raiondesu@gmail.com>
2020-09-04 12:36:23 +0300

    Update dist

--- dist/esnext/index.js
+++ dist/esnext/index.js
@@ -1,6 +1,14 @@
-export const prop = (options) => options;
-export const withProps = (props, setup) => {
-    setup.props = props;
-    return setup;
-};
+export const prop = () => (options) => options;
+;
+export const component = (name, inheritAttrs = true, props, events) => ({
+    withProps: props => component(name, inheritAttrs, props, events),
+    emits: events => component(name, inheritAttrs, props, events),
+    setup: setup => (setup.inheritAttrs = inheritAttrs,
+        setup.displayName = name,
+        setup.emits = events,
+        setup.props = props,
+        setup)
+});
+export const withProps = (props, setup) => (setup.props = props,
+    setup);
 //# sourceMappingURL=index.js.map
--- dist/js/index.js
+++ dist/js/index.js
@@ -1,9 +1,17 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.withProps = exports.prop = void 0;
-exports.prop = (options) => options;
-exports.withProps = (props, setup) => {
-    setup.props = props;
-    return setup;
-};
+exports.withProps = exports.component = exports.prop = void 0;
+exports.prop = () => (options) => options;
+;
+exports.component = (name, inheritAttrs = true, props, events) => ({
+    withProps: props => exports.component(name, inheritAttrs, props, events),
+    emits: events => exports.component(name, inheritAttrs, props, events),
+    setup: setup => (setup.inheritAttrs = inheritAttrs,
+        setup.displayName = name,
+        setup.emits = events,
+        setup.props = props,
+        setup)
+});
+exports.withProps = (props, setup) => (setup.props = props,
+    setup);
 //# sourceMappingURL=index.js.map
